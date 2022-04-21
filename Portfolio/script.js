331acc3f7908e22312643d01128cf91cf0ef7223
mthiagoalves <m.thiago.ales@live.com>
2022-04-21 12:05:01 -0300

    refatoramento do portfolio

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,9 +1,6 @@
-let menuToggle = document.querySelector('.toggle');
-let navigation =document.querySelector('.navigation')
-let sec = document.querySelector('.sec')
-
-menuToggle.onclick = function (){
-    menuToggle.classList.toggle('active');
-    navigation.classList.toggle('active');
-    sec.classList.toggle('active');
+toggleMenu = () => {
+    const toggleMenu = document.querySelector('.toggle');
+    const sidebar = document.querySelector('.sidebar');
+    toggleMenu.classList.toggle('active');
+    sidebar.classList.toggle('active');
 }
